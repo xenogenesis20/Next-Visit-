bff4944f881fea2bb1399a00ce6726140898eb89
xenogenesis20 <xenogenesis20@outlook.com>
2021-06-19 17:12:54 +0000

    working on the singup page

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,18 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			userInfo: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
--- src/js/views/SignUp.js
+++ src/js/views/SignUp.js
@@ -1,43 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import * as mdb from "mdb-ui-kit"; // lib
+import { Context } from "../store/appContext";
 
 export const SignUp = () => {
+	const { store, actions } = useContext(Context);
+
 	useEffect(() => {
 		document.querySelectorAll(".form-outline").forEach(formOutline => {
 			new mdb.Input(formOutline).update();
 		}, []);
 	});
 	return (
-		<div>
-			<div className="container form-wrapper">
-				<div>
-					<h1 className="text-center mt-5">Lets collect some basic information.</h1>
-					<form>
-						<div className="form-outline bg-light mb-3">
-							<input type="text" id="form1" className="form-control" />
-							<label className="form-label" htmlFor="form1">
-								Full Name
-							</label>
-						</div>
-						<div className="input-group">
-							<label>Email</label>
-							<input type="email" className="form-control" name="email" placeholder="Enter email" />
-						</div>
-						<div className="input-group">
-							<label>Phone</label>
-							<input type="phone" className="form-control" name="address" placeholder="Enter phone" />
-						</div>
-						<div className="input-group">
-							<label>Address</label>
-							<input type="text" className="form-control" name="phone" placeholder="Enter address" />
-						</div>
-
-						<button type="button" className="btn btn-primary btn-rounded form-control">
-							save
-						</button>
-					</form>
+		<div className="container signup-box">
+			<h4 className="text-center mt-2 p-3">Lets collect some basic information.</h4>
+			<form>
+				<div className="form-outline bg-light mb-3 p-1">
+					<input type="text" id="name-input" className="form-control" />
+					<label className="form-label" htmlFor="name-input">
+						Full Name
+					</label>
 				</div>
-			</div>
+				<div className="form-outline bg-light mb-3 p-1">
+					<input type="text" id="email-input" className="form-control" />
+					<label className="form-label" htmlFor="email-input">
+						Email
+					</label>
+				</div>
+				<div className="form-outline bg-light mb-3 p-1">
+					<input type="number" id="phone-input" className="form-control" />
+					<label className="form-label" htmlFor="phone-input">
+						Phone
+					</label>
+				</div>
+				<div className="form-outline bg-light mb-3 p-1">
+					<input type="text" id="address-input" className="form-control" />
+					<label className="form-label" htmlFor="address-input">
+						Email
+					</label>
+				</div>
+
+				<button type="button" className="btn btn-primary btn-rounded form-control">
+					save
+				</button>
+			</form>
 		</div>
 	);
 };
