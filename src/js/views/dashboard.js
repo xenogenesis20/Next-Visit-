5c8d05144db7e87c08088a53c87577a44c112658
N-Archer <archer.nj@gmail.com>
2021-06-22 22:51:17 +0000

    Dashboard View added

---
+++ src/js/views/dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../styles/home.scss";
+import { Header } from "../component/Header";
+import { Footer } from "../component/footer";
+
+export const Dashboard = () => {
+	return (
+		<>
+			<header className="wrapper">
+				<Header />
+			</header>
+			<div>
+				
+			</div>
+		</>
+	);
+};
