39cb01e9e0c195865833cc9b2b3996d30283a2ab
xenogenesis20 <xenogenesis20@outlook.com>
2021-06-25 19:09:31 +0000

    fixed some problems but navbar/body positiong is weird?

--- src/js/component/SectionCardFeaturesThree.js
+++ src/js/component/SectionCardFeaturesThree.js
@@ -3,23 +3,28 @@ import { Link } from "react-router-dom";
 
 export const SectionCardFeaturesThree = () => {
 	return (
-		<div className="about-card">
-			<div className="card text-center" style={{ width: "23rem" }}>
+		<div className="card text-center " style={{ backgroundColor: "#f5f5ff" }}>
+			<div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
 				<img
 					src="https://image.freepik.com/free-vector/holistic-medicine-abstract-concept-vector-illustration-alternative-natural-medicine-holistic-mental-therapy-whole-body-treatment-health-practice-disease-integrative-doctor-abstract-metaphor_335657-1696.jpg"
-					className="card-img-top"
-					alt="..."
+					className="img-fluid"
 				/>
-				<div className="card-body">
-					<h4 className="card-title">View all of your information on the dashboard</h4>
-					<p className="card-text">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae mollitia deserunt quod
-						eligendi exercitationem dignissimos! Numquam nulla asperiores modi, unde, voluptatum nam
-						inventore at facilis deleniti provident incidunt nostrum et debitis veniam quas? Nobis
-						architecto quos minus ad, deserunt ab odio cum, iusto id at recusandae pariatur animi aut
-						commodi!{" "}
-					</p>
-				</div>
+				<a href="#!">
+					<div className="mask" style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }} />
+				</a>
+			</div>
+
+			<div className="card-body">
+				<h5 className="card-title">View all of your information on the dashboard</h5>
+				<p className="card-text">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae mollitia deserunt quod eligendi
+					exercitationem dignissimos! Numquam nulla asperiores modi, unde, voluptatum nam
+				</p>
+				<Link to="/SignUp">
+					<button type="button" className="btn btn-primary">
+						SIGN UP!
+					</button>
+				</Link>{" "}
 			</div>
 		</div>
 	);
