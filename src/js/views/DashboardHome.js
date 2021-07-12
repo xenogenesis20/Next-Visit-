6ae0653c962ab9d66d1dded5102e28fefef1cb14
xenogenesis20 <xenogenesis20@outlook.com>
2021-07-12 22:53:39 +0000

    pizza is ready

--- src/js/views/DashboardHome.js
+++ src/js/views/DashboardHome.js
@@ -5,6 +5,7 @@ import { SmallMedCard } from "../component/SmallMedCard";
 import { GlobalState } from "../store/appContext";
 import PropTypes from "prop-types";
 import { Redirect } from "react-router-dom";
+import { BarGraph } from "../component/BarGraph";
 
 export const DashboardHome = props => {
 	const { store, actions } = useContext(GlobalState);
@@ -12,7 +13,7 @@ export const DashboardHome = props => {
 	return (
 		<div className="container-fluid m-0 p-0 d-flex linear-bg">
 			<div className="side-nav">
-				<SideNav />
+				<SideNav loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn} />
 			</div>
 			<div className="container-fluid p-0 m-0">
 				<div className="row g-0 p-0 m-0 med-and-symptom-row">
@@ -31,6 +32,9 @@ export const DashboardHome = props => {
 				</div>
 				<div className="row charts-row g-0 p-0 m-0">
 					<div className="col-12 ">Charts go here</div>
+					<div>
+						<BarGraph />
+					</div>
 				</div>
 			</div>
 			{props.loggedIn ? "" : <Redirect to="SignIn" />}
