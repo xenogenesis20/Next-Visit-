b9eb5e89f4e78ca05bb430336f12f9aba903f7f9
xenogenesis20 <xenogenesis20@outlook.com>
2021-07-12 19:51:15 +0000

    login / log out now working

--- src/js/views/DashboardVitals.js
+++ src/js/views/DashboardVitals.js
@@ -2,16 +2,23 @@ import React from "react";
 import "../../styles/dashboard.scss";
 import { SideNav } from "../component/SideNav";
 import { YourVitals } from "../component/YourVitals";
+import PropTypes from "prop-types";
+import { Redirect } from "react-router-dom";
 
-export const DashboardVitals = () => {
+export const DashboardVitals = props => {
 	return (
 		<div className="dashboard-wrapper d-flex">
 			<div className="side-nav">
-				<SideNav />
+				<SideNav loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn} />
 			</div>
 			<div className="medication-column">
 				<YourVitals />
 			</div>
+			{props.loggedIn ? "" : <Redirect to="SignIn" />}
 		</div>
 	);
+};
+DashboardVitals.propTypes = {
+	loggedIn: PropTypes.bool,
+	setLoggedIn: PropTypes.func
 };
