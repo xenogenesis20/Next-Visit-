9495d6c4efa594b75ff22b587fcc1f0d7e7ae376
xenogenesis20 <xenogenesis20@outlook.com>
2021-07-28 17:52:01 +0000

    more css

--- src/js/component/SingleMedicationCard.js
+++ src/js/component/SingleMedicationCard.js
@@ -18,28 +18,31 @@ export const SingleMedicationCard = props => {
 		actions.editUserMedication(med);
 	};
 
-	useEffect(() => {
-		let str = props.entity.name;
+	useEffect(
+		() => {
+			let str = props.entity.name;
 
-		let arr = str.split("");
+			let arr = str.split("");
 
-		let newStr = arr[0].trim();
+			let newStr = arr[0].trim();
 
-		fetch(`https://api.fda.gov/drug/label.json?search=adverse_reactions:${newStr}`)
-			.then(function(response) {
-				if (!response.ok) {
-					throw Error(response.statusText);
-				}
-				return response.json();
-			})
-			.then(function(responseAsJson) {
-				// console.log("response log", responseAsJson);
-				setMedicationData(responseAsJson.results);
-			})
-			.catch(function(err) {
-				console.log("Fetch Error :-S", err);
-			});
-	}, [medications]);
+			fetch(`https://api.fda.gov/drug/label.json?search=adverse_reactions:${newStr}`)
+				.then(function(response) {
+					if (!response.ok) {
+						throw Error(response.statusText);
+					}
+					return response.json();
+				})
+				.then(function(responseAsJson) {
+					// console.log("response log", responseAsJson);
+					setMedicationData(responseAsJson.results);
+				})
+				.catch(function(err) {
+					console.log("Fetch Error :-S", err);
+				});
+		},
+		[medications]
+	);
 	// console.log("Props Entity", props.entity);
 	console.log("Props Entity", props.entity.id);
 
